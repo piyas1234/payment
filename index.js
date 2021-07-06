f745295ecf90815c90a87d949367476a18939f10
piyas1234 <mypiyastv@gmail.com>
2021-07-06 17:18:44 +0600

    all

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const SSLCommerzPayment = require('sslcommerz')
+require('dotenv').config()
+const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+
+app.use(bodyParser.json())
+
+
+app.use("/ssl-request" , async (req,res,next)=>{
+
+    const data = {
+        total_amount: 100,
+        currency: 'EUR',
+        tran_id: 'REF123',
+        success_url: 'http://localhost:2000/ssl-payment-success/',
+        fail_url: 'http://localhost:2000/ssl-payment-failure',
+        cancel_url: 'http://localhost:2000/ssl-payment-cancel',
+        ipn_url: 'http://localhost:2000/ssl-payment-ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'cust@yahoo.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        multi_card_name: 'mastercard',
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D'
+    };
+    const sslcommer = new SSLCommerzPayment('taluk60e42fc905140', 'taluk60e42fc905140@ssl', false)  
+    sslcommer.init(data).then(data => {
+        if(data?.GatewayPageURL){
+            return res.status(200).redirect(data.GatewayPageURL)
+        }else{
+            return res.status(400).json({
+                message:"Ssl session was not successful"
+            })
+        }
+         
+    });
+})
+
+
+
+
+app.post("/ssl-payment-success",async(req,res,next)=>{
+    return res.status(200).json({
+        data: req.body
+    })
+});
+
+app.post("/ssl-payment-failure",async(req,res,next)=>{
+    return res.status(400).json({
+        data: req.body
+    })
+});
+
+
+app.post("/ssl-payment-cancel",async(req,res,next)=>{
+    return res.status(400).json({
+        data: req.body
+    })
+});
+app.post("/ssl-payment-ipn",async(req,res,next)=>{
+    return res.status(400).json({
+        data: req.body
+    })
+});
+
+app.use("/",(req,res)=>{
+    res.status(200).json({
+        "name":"home page"
+    })
+})
+ 
+
+const port = 2000 || process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
